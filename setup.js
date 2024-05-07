d5e87af6f700b1304a44d7aede7890c9aed034dd
4KRO5 <4kro54k@gmail.com>
2024-05-06 17:46:17 -0700

    Centrado de canvas

--- setup.js
+++ setup.js
@@ -1,12 +1,15 @@
 let inputHoraLaPaz;
 
 function setup() {
-    createCanvas(800, 400);
+    let canvasX = (window.innerWidth - 800) / 2;
+    let canvasY = (window.innerHeight - 400) / 2;
+    createCanvas(800, 400).position(canvasX, canvasY);
+
     background("white");
     angleMode(DEGREES);
 
     inputHoraLaPaz = createInput('11:59', 'time');
-    inputHoraLaPaz.position(20, 20);
+    inputHoraLaPaz.position(canvasX + 20, canvasY + 20);
     inputHoraLaPaz.input(updateHoraLaPaz);
     frameRate(1);
 }
